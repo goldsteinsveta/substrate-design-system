d34b777f50098ce4f3b7d1d24fc26e46f9d91a9a
goldsteinsveta <svetlana.goldstein@gmail.com>
2020-02-17 13:22:10 +0100

    Knobs for TableAccounts

--- src/components/Tables/TableAccounts.stories.js
+++ src/components/Tables/TableAccounts.stories.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { withKnobs, object } from '@storybook/addon-knobs';
+
 import { TableAccounts } from './TableAccounts';
 import { mockupBondData, bondsColumns } from '../dataMockups/accountBonds';
 
 export default {
   title: 'Design System/TableAccounts',
   component: TableAccounts,
+  decorators: [withKnobs],
 };
 
-export const all = () => <TableAccounts columns={bondsColumns} data={mockupBondData(5)} />;
+export const knobbedTableAccounts = () => {
+  const data = object('data', mockupBondData(1));
+  const columns = object('columns', bondsColumns);
+
+  return <TableAccounts columns={columns} data={data} />;
+};
+
+export const tableAccounts = () => (
+  <TableAccounts columns={bondsColumns} data={mockupBondData(5)} />
+);
